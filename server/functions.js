ba9f8945e2043074391d9e768d5f9e0d1cb6a46e
Ocean <oeverspe@uncc.edu>
2019-05-12 17:25:25 +0900

    Graphql query v1 complete

--- server/functions.js
+++ server/functions.js
@@ -1,12 +1,17 @@
 // import axios to make graphql call
 const axios = require('axios');
 const db = require('../db')
-// import 
+// import
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
 const lineItemPagination = async (order_id, cursor, dataArray, accessToken, shop) => {
-    // gql query 
-
+    // gql query
+    console.log(`query variables: order id ${order_id}, line item cursor ${cursor}`)
     try {
+      console.log('waiting...')
         const res = await axios({
             url: `https://kabir-test.myshopify.com/admin/api/graphql.json`,
             method: 'post',
@@ -17,7 +22,7 @@ const lineItemPagination = async (order_id, cursor, dataArray, accessToken, shop
                         order(id: "gid://shopify/Order/${order_id}") {
                             createdAt
                             id
-                            lineItems(first:10,${cursor ? `after:${cursor}` : ''}) {
+                            lineItems(first:1 ${cursor ? `, after:"${cursor}"` : ''} ) {
                                 edges {
                                     cursor
                                     node {
@@ -37,24 +42,89 @@ const lineItemPagination = async (order_id, cursor, dataArray, accessToken, shop
         })
         // save product data
         // edges[0].products.id
-        console.log(res.data.data.order.lineItems.edges)
-        for (const lineItem of res.data.data.orders.lineItems.edges) {
+        console.log('\ninside extra item query')
+        for (const lineItem of res.data.data.order.lineItems.edges) {
             console.log(lineItem)
-            console.log(lineItem.node.product.id.slice(22))
-            // dataArray.push(lineItem.node.product.id.slice(22))
+            dataArray.push(lineItem.node.product.id.slice(22))
         }
-        
-        if (res.data.data.orders.lineItems.pageInfo.hasNextPage) {
-            let cursor = res.data.data.orders.lineItems.edges.cursor
+
+        if (res.data.data.order.lineItems.pageInfo.hasNextPage) {
+            let finalIdx = res.data.data.order.lineItems.edges.length - 1
+            let nextCursor = res.data.data.order.lineItems.edges[finalIdx].cursor
+            console.log(`lineItems next page, cursor ${nextCursor}`)
+
             // [].push(lineItemPagination(order_id, cursor).data)
-            dataArray.concat(lineItemPagination(order_id, cursor, dataArray, accessToken, shop))
+            dataArray.concat(await lineItemPagination(order_id, nextCursor, dataArray, accessToken, shop))
         }
         return dataArray
     } catch(err) {
-        console.log(err.stack)
+        console.log(`failed in lineitems check ${err.stack}`)
     }
 
-    // has next page ? then lineItemPagination 
+    // has next page ? then lineItemPagination
+
+}
+
+const ordersQuery = async (shop, accessToken, cursor='') => {
+  console.log('\nTrying orders Query')
+  try {
+    let res = await axios({
+        url: `https://${shop}/admin/api/graphql.json`,
+        method: 'post',
+        headers: { 'X-Shopify-Access-Token': accessToken },
+        data: {
+            query: `
+                {
+                    orders(first:10, ${cursor ? `after:"${cursor}",` : ''} query:"created_at:>#{2018-01-01T00:00:01}") {
+                        edges {
+                        cursor
+                            node {
+                                createdAt
+                                id
+                                lineItems(first:1) {
+                                    edges {
+                                        cursor
+                                        node {
+                                            product {
+                                                id
+                                            }
+                                        }
+                                    }
+                                    pageInfo {
+                                        hasNextPage
+                                    }
+                                }
+                            }
+                        }
+                        pageInfo {
+                            hasNextPage
+                        }
+                    }
+                }
+            `
+        }
+    })
+    let ordersArray = res.data.data.orders.edges
+    let ordersCursor = res.data.data.orders.edges.cursor
+    let ordersPaginate = res.data.data.orders.pageInfo.hasNextPage // boolean
+
+    console.log("ordersPaginate ", ordersPaginate)
+
+    console.log("CURRENT THROTTLE LIMIT", res.data.extensions.cost.throttleStatus.currentlyAvailable)
+    if(res.data.extensions.cost.throttleStatus.currentlyAvailable<500){
+      let timetowait = Math.round( parseInt(res.data.extensions.cost.requestedQueryCost) / 50)
+      console.log(timetowait)
+      await sleep(timetowait * 1000)
+      console.log("SLEPT")
+    }
+
+    if (ordersPaginate) {ordersArray.concat(await ordersQuery(shop, accessToken, ordersCursor))}
+
+    return ordersArray
+
+  } catch(err) {
+    console.log(`failed in orders check ${err.stack}`)
+  }
 
 }
 
@@ -62,9 +132,9 @@ module.exports = {
 
     customProductRank: (start, end) => {
         console.log("custom product rank", start, end)
-        // query database directly 
+        // query database directly
     },
-    buildDatabase: async (shop, accessToken, cursor='') => {
+    buildDatabase: async (shop, accessToken) => {
         // might need to add a cursor argument for pagination
         // need to have the "created at" for the next day's query be the time the last query started
         // save the current time on query
@@ -74,46 +144,13 @@ module.exports = {
         let queryObj = {
             order_id: '',
             product_id: '',
-            month: null, 
+            month: null,
             week: null,
             created_at: ''
         }
         try {
-            const res = await axios({
-                url: `https://${shop}/admin/api/graphql.json`,
-                method: 'post',
-                headers: { 'X-Shopify-Access-Token': accessToken },
-                data: {
-                    query: `
-                        {
-                            orders(first:20,${cursor ? `after:${cursor},` : ''}query:"created_at:>#{2019-01-01T00:00:01}") {
-                                edges {
-                                cursor
-                                    node {
-                                        createdAt
-                                        id
-                                        lineItems(first:1) {
-                                            edges {
-                                                node {
-                                                    product {
-                                                        id
-                                                    }
-                                                }
-                                            }
-                                            pageInfo {
-                                                hasNextPage
-                                            }
-                                        }
-                                    }
-                                }
-                                pageInfo {
-                                    hasNextPage
-                                }
-                            }
-                        }
-                    `
-                }
-            })
+            const ordersArray = await ordersQuery(shop, accessToken)
+            console.log('\nCompleted orders array', ordersArray)
             // await buildDatabase(res.data)
             // console.log(res.data.data.orders.edges) // array of elements that contain node property
             /*
@@ -125,13 +162,13 @@ module.exports = {
             console.log(res.data.data.orders.edges[0].node.lineItems.edges[0].node.product.id) // 'gid://shopify/Product/2112769884227'
 
             order_id, product_id, month, week, order_created_at
-            10000000, 1111111111,     1,    2, 
+            10000000, 1111111111,     1,    2,
             10000000, 2222222222,     1,    2, 2019-01-10T03:25:48Z
             19999999, 1010101010,     2,    7, 2019-02-21T03:28:48Z
             19999999, 2222222222,     2,    7, 2019-02-21T03:28:48Z
 
             need to insert on each iteration of line items because the database can look like
-            the query object way can't handle multiple inserts 
+            the query object way can't handle multiple inserts
 
             */
             // if we are pushing values when we get them, the order of the column rows should change so the values array is correct
@@ -139,15 +176,15 @@ module.exports = {
             // can have an object that persists through the loop with all the values we need to insert at the end in the right place
             // order created at or product created at?
             //console.log(res.data.data.orders.edges[0.node.lineItems.edges[0]])
-            const ordersArray = res.data.data.orders.edges
-            const ordersPaginate = res.data.data.orders.pageInfo.hasNextPage // boolean
-            let queryObj = {
-                text: 'INSERT INTO orders_products (order_id, product_id, month, week, created_at) VALUES($1, $2, $3, $4, $5)',
-                values: [],
-            }
+
+            // let queryObj = {
+            //     text: 'INSERT INTO orders_products (order_id, product_id, month, week, created_at) VALUES($1, $2, $3, $4, $5)',
+            //     values: [],
+            // }
 
             ordersArray.forEach(async order => { // for each order
 
+                console.log("\nORDER START")
                 let orderID = order.node.id.slice(20)
                 console.log("orderID", orderID) // add to queryObj
                 let orderCreatedAt = order.node.createdAt
@@ -155,46 +192,59 @@ module.exports = {
                 // date calculation
 
                 let lineItemsPaginate = order.node.lineItems.pageInfo.hasNextPage; // boolean
-                let cursor = order.cursor 
+                let cursor = order.cursor
+                let lineItemCursor = ''
                 let resArray = order.node.lineItems.edges
                 let lineItemsArray = []
 
                 // deal with the first page of line items
-                resArray.forEach(lineItem => {
-                    console.log(lineItem)
-                    // lineItemsArray.push(lineItem.node.product.id.slice(22))
+                resArray.forEach((lineItem, index, array) => {
+                  lineItemsArray.push(lineItem.node.product.id.slice(22))
+
+                  if ( index === array.length - 1) {
+                    lineItemCursor = lineItem.cursor
+                    console.log(`pre paginated line items array ${lineItemsArray}`)
+                  }
                 });
 
                 // paginate the remaining line items
                 if (lineItemsPaginate) {
-                    console.log("NEXT PAGE")
-                    let concatArray = await lineItemPagination(orderID, cursor, [], accessToken, shop)
-                    lineItemsArray.concat(concatArray)
+                    console.log(`NEXT PAGE ${lineItemsPaginate}`)
+                    console.log(`lineItem next page cursor ${lineItemCursor}`)
+                    let concatArray = await lineItemPagination(orderID, lineItemCursor, [], accessToken, shop)
+                    lineItemsArray = lineItemsArray.concat(concatArray)
+                    console.log("paginated line items ", )
                     // let paginatedArray = await lineItemPagination(orderID, cursor, [])
+                    lineItemsArray.forEach(lineItem => { // for each line item
+
+                        console.log(`NESTED PRODUCT IDS ${lineItem}
+                          `)
+                        // queryObj
+                        // orderid 4334234
+                        // orderid 4232343
+                        // build array of objects
+
+                        // if lineItemsPaginate paginate line items
+                    });
+                } else {
+
+                  lineItemsArray.forEach(lineItem => { // for each line item
+                      let productID = lineItem
+
+                      console.log(`PRODUCT ID ${productID}
+                        `)
+                      // queryObj
+                      // orderid 4334234
+                      // orderid 4232343
+                      // build array of objects
+
+                      // if lineItemsPaginate paginate line items
+                  });
                 }
 
-                // line item pagination function
-                // lineItemPagination(x, x, [])
-
-                
-
-                lineItemsArray.forEach(lineItem => { // for each line item
-                    let productID = lineItem.node.product.id.slice(22)
-
-                    console.log("productID", productID)
-                    // queryObj
-                    // orderid 4334234
-                    // orderid 4232343
-                    // build array of objects
-
-                    // if lineItemsPaginate paginate line items
-                });
-
-                // if ordersPaginate then paginate 
-                // 
-
+                console.log("ORDER FINISHED")
             });
-            // await db.query(myObj) function
+
         } catch(err) {
             console.log(err.stack)
         }
